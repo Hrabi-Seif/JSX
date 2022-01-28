f9983c4fbb47577cf0f545a6fe367e0c6bb5e44d
hrabi-seif <harrabi.seife@gmail.com>
2022-01-28 18:39:46 +0100

    jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import img from "../src/assets/1.jpeg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="maria" style={{ maxWidth: "100vw" }}>
+        <h1 className="title">Tennis player</h1>
+        <div id="back">
+          <img src="maria.png" alt="maria"></img>
+          <br />
+          <img src={img} alt="maria"></img>
+        </div>
+        <br />
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/q3LiBvA4trI?autoplay=1"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   );
 }
